8e42024baf6c3c0bd9730e694986d027937cb8df
Aryan <ranaaryan2100@gmail.com>
2024-06-27 13:21:45 +0530

    second commit

---
+++ problem-014/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        unordered_map<int,int> hash;
+
+    for(auto it: edges){
+        hash[it[0]]++;
+        hash[it[1]]++;
+    }
+    for(auto it : hash){
+        int node = it.first;
+        int degree = it.second;
+        if(degree == edges.size()){
+            return node;
+        }
+    }
+    return -1;
+    }
+};
